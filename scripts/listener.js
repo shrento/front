8271c9018468d6765f0b9dc5b0c3cd0d884c1b64
Shrento <s.shapran2016@yandex.ru>
2024-10-31 08:58:54 +0300

    implemented lab4

---
+++ scripts/listener.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    let loadingTime = performance.now();
+    let message = `Страница загружена за ${loadingTime.toFixed(2)} миллисекунд`;
+    let footer = document.getElementsByTagName('footer')[0];
+    footer.innerHTML += `<p>${message}</p>`;
+});
